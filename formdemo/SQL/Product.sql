77cc3891269303bcaa2360f2d16c8630043f9ba9
suriarasai <suria.r.asai@gmail.com>
2019-05-20 06:48:02 +0800

    SQL

---
+++ formdemo/SQL/Product.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sa48`.`product` (
+  `id` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(50) NULL,
+  `type` VARCHAR(50) NULL,
+  `category` VARCHAR(50) NULL,
+  `description` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`));
